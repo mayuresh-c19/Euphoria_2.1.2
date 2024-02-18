7f3502416b5cb9062d1f5927130376a512bec39c
mayuresh-c19 <mayuresh1918@gmail.com>
2024-02-18 17:43:37 +0530

    Added Audio Files

--- app/pages/landing.tsx
+++ app/pages/landing.tsx
@@ -8,12 +8,12 @@ const Landing = () => {
     return ( 
         <div>
             <Navbar />
-            <div className="mt-72">
-                        <h1 className="text-3xl flex justify-center items-center font-bold ">Welcome to BeatMarket</h1>
+            <div className="justify-center items-center grid mt-72">
+                        <h1 className="text-3xl flex font-bold ">Welcome to BeatMarket</h1>
                         <h2 className="text-lg flex justify-center items-center">The best place to find and buy beats</h2>
                         <Button variant="destructive" className="mx-auto justify-center flex items-center">
                             Get Started 
-                        </Button>
+                        </Button>   
             </div>
                 
         </div>
