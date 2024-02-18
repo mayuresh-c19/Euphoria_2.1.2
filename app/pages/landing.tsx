904768949f9973e733582ebc106c062de34cacda
mayuresh-c19 <mayuresh1918@gmail.com>
2024-02-19 01:05:42 +0530

    Database Connected

--- app/pages/landing.tsx
+++ app/pages/landing.tsx
@@ -2,23 +2,21 @@ import React from 'react';
 import { Navbar } from '@/components/landingnavbar';
 import { Button } from '@/components/ui/button';
 import { useNavigate } from 'react-router-dom';
-
-
+import bgImage from '../assets/bg2.jpg';
 
 const Landing = () => {
     const navigate = useNavigate();
     return ( 
         <div>
             <Navbar />
-            <div className="justify-center items-center mt-72 bg-cover bg-center h-screen" style={{ backgroundImage: 'url("bg1.jpg")' }}>
-                        <h1 className="text-5xl flex justify-center items-center font-serif">Welcome to BeatMarket</h1>
-                        <h2 className="text-lg flex justify-center items-center text-orange-500">The best place to find and buy beats</h2>
-                        <Button variant="destructive" className="mx-auto justify-center flex items-center" onClick={() => navigate('/signup')}>
-                            Get Started 
-                        </Button>
-
+            <div className="flex flex-col justify-center items-center mt-72 h-screen bg-center bg-cover" style={{ backgroundImage: `url(${bgImage})` }}>
+                <h1 className="text-5xl font-serif">Welcome to BeatMarket</h1>
+                <h2 className="text-lg text-orange-500">The best place to find and buy beats</h2>
+                <Button variant="destructive" className="mt-4" onClick={() => navigate('/signup')}>
+                    Get Started 
+                </Button>
             </div>
-            </div>   
+        </div>   
      );
 }
  
