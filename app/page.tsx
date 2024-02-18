e4943c654e69798c1906810a9da0e53d0fdad30e
Omkar Gaikwad <omgaikwad1411@gmail.com>
2024-02-18 16:11:26 +0530

    Aded forgot password page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './pages/landing';
 import Login from './pages/login';
 import Signup from './pages/signup';
+import ForgotPass from '../app/pages/forgotpass'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/forgotpass" element={<ForgotPass />} />
       </Routes>
     </Router>
   );
